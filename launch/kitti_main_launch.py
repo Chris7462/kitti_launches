90758acddcbd5c6cd2285fdbb000496fa569e3b8
chris7462 <chris7462@gmail.com>
2023-12-28 14:28:37 -0500

    Fix coding style

--- launch/kitti_main_launch.py
+++ launch/kitti_main_launch.py
@@ -1,39 +1,42 @@
+from os.path import join
+
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
-from launch_ros.actions import Node, SetParameter
 from launch.actions import ExecuteProcess, IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution
-from ament_index_python.packages import get_package_share_directory
-from os.path import join
+from launch_ros.actions import Node, SetParameter
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
     bag_exec = ExecuteProcess(
-        cmd=["ros2", "bag", "play", "-r", "1.0", "/data/Kitti/raw/2011_09_29_drive_0071_sync_bag" , "--clock"]
+        cmd=['ros2', 'bag', 'play', '-r', '1.0',
+             '/data/Kitti/raw/2011_09_29_drive_0071_sync_bag' , '--clock']
     )
 
     # The TF and URDF of the vehicle
     robot_state_publisher_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([
-                FindPackageShare("kitti_urdf"), "launch", "kitti_urdf_launch.py"
+                FindPackageShare('kitti_urdf'), 'launch', 'kitti_urdf_launch.py'
             ])
         ])
     )
 
     rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        arguments=["-d", join(get_package_share_directory("kitti_launches"), "rviz", "kitti.rviz")]
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', join(get_package_share_directory('kitti_launches'), 'rviz', 'kitti.rviz')]
     )
 
     # Localization launch
     localization_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([
-                FindPackageShare("kitti_launches"), "launch", "localization_launch.py"
+                FindPackageShare('kitti_launches'), 'launch', 'localization_launch.py'
             ])
         ])
     )
@@ -41,7 +44,7 @@ def generate_launch_description():
     # Perception launch
 
     return LaunchDescription([
-        SetParameter(name="use_sim_time", value=True),
+        SetParameter(name='use_sim_time', value=True),
         bag_exec,
         robot_state_publisher_launch,
         rviz_node,
