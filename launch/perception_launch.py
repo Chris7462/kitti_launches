357bee562ec6eae03a0e312834e0d98156335c65
chris7462 <chris7462@gmail.com>
2025-09-04 12:24:34 -0400

    update perception launch file

--- launch/perception_launch.py
+++ launch/perception_launch.py
@@ -1,20 +1,56 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    yolo_object_detection_launch = IncludeLaunchDescription(
+    declare_use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (bagfile) clock if true'
+    )
+
+    fcos_object_detection_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([
-                FindPackageShare('yolo_object_detection'), 'launch',
-                'yolo_object_detection_launch.py'
+                FindPackageShare('fcos_object_detection'), 'launch',
+                'fcos_object_detection_launch.py'
             ])
-        ])
+        ]),
+        launch_arguments={
+            'use_sim_time': LaunchConfiguration('use_sim_time')
+        }.items()
+    )
+
+    sort_tracker_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('sort_tracker'), 'launch',
+                'sort_tracker_launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'use_sim_time': LaunchConfiguration('use_sim_time')
+        }.items()
+    )
+
+    fcn_segmentation_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('fcn_segmentation'), 'launch',
+                'fcn_segmentation_launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'use_sim_time': LaunchConfiguration('use_sim_time')
+        }.items()
     )
 
     return LaunchDescription([
-        yolo_object_detection_launch
+        declare_use_sim_time,
+        fcos_object_detection_launch,
+        sort_tracker_launch,
+        fcn_segmentation_launch
     ])
